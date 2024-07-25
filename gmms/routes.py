3f3df65c7647e3c327e1c1d62199ce1c6532032c
Hassaan Abbasi <and182@txstate.edu>
2024-07-25 02:38:30 -0500

    Updating Login-Page

--- gmms/routes.py
+++ gmms/routes.py
@@ -1,5 +1,22 @@
 from gmms import app
+from flask import render_template
+
+#@app.route('/')
+#def hello():
+#    return "Hello World!"
 
 @app.route('/')
-def hello():
-    return "Hello World!"
+def index():
+    return render_template("index.html")
+
+@app.route('/customer')
+def Customer():
+    return render_template("customer.html")
+
+@app.route('/technician')
+def Technician():
+    return render_template("technician.html")
+
+@app.route('/approver')
+def Approver():
+    return render_template("approver.html")
