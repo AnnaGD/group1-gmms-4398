a50889f3e1d9f7534fa786e59a5e5ad103ecbebb
Anna Goncharova <adn74@txstate.edu>
2024-07-25 20:11:49 -0500

    Quick correction

--- gmms/models/__init__.py
+++ gmms/models/__init__.py
@@ -5,4 +5,4 @@ db = SQLAlchemy()
 def init_app(app):
     db.init_app(app)
 
-# eg: for from .models import WorkRequest  # Assuming WorkRequest is a class in models.py
+# eg case -- :  from .models import WorkRequest  # Assuming WorkRequest is a class in models.py
